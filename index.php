fdc816991b67207e099806b0dbcc190c0396f976
Little Bear <softganz@gmail.com>
2016-09-04 13:47:25 +0700

    After host edit

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ echo "Hello world. Git<br />";
 echo "Welcome home<br />";
 echo "From Little Bear<br />";
 echo "On Sep 4,2016 13:32PM<br />";
-<<<<<<< Updated upstream
 echo "Host edit";
-=======
 echo "After host edit";
->>>>>>> Stashed changes
 ?>
