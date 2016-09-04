6d39adaa4d596d06fc9e7852d9e94c87cdadcb1a
Little Bear <softganz@gmail.com>
2016-09-04 14:17:39 +0700

    On 14:17PM

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ echo "Hello world. Git<br />";
 echo "Welcome home<br />";
 echo "From Little Bear<br />";
 echo "On Sep 4,2016 13:32PM<br />";
-echo "Host edit";
-echo "After host edit";
-echo "On Sep 4,2016 13:49PM";
-echo "On Sep 4,2016 13:50PM";
+echo "Host edit<br />";
+echo "After host edit<br />";
+echo "On Sep 4,2016 13:49PM<br />";
+echo "On Sep 4,2016 13:50PM<br />";
+echo "On Sep 4,2016 14:08PM<br />";
 ?>
